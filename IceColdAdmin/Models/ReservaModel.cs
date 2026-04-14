e4567755a0d34b4925d0abf8ecb3e673a9d69b62
jordi <jordijrbercero@gmail.com>
2026-04-14 12:19:08 +0200

    Agregar archivos de proyecto.

---
+++ IceColdAdmin/Models/ReservaModel.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace IceColdAdmin.Models 
+{
+    public class ReservaModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        
+        public string ClienteNombre { get; set; }
+        public string ClienteEmail { get; set; }
+
+        
+        public string Servicio { get; set; }
+        public string Barbero { get; set; }
+        public string Dia { get; set; }
+        public string Hora { get; set; }
+        public string Precio { get; set; }
+        public DateTime FechaCreacion { get; set; }
+
+        
+        public bool email_confirmacion_enviado { get; set; }
+        public bool recordatorio_enviado { get; set; }
+
+        
+        public string fecha_confirmacion { get; set; }
+        public string fecha_recordatorio { get; set; }
+    }
+}
